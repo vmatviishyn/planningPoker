698e73b1bccb9f9f0e5d7c189ee07f9e46ed62b5
Taras Moskovych <Taras_Moskovych@epam.com>
2019-10-11 16:48:23 +0300

    add environment to .gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBYWVKlMQACqQkO-Sn1sUEfZ1YgE7FL_JM',
-    authDomain: 'planing-pocker.firebaseapp.com',
-    databaseURL: 'https://planing-pocker.firebaseio.com',
-    projectId: 'planing-pocker',
-    storageBucket: 'planing-pocker.appspot.com',
-    messagingSenderId: '623244221117',
-    appId: '1:623244221117:web:212ef206cd495468c375d4',
-    measurementId: 'G-KVHT2GN784'
+    // Add your firebase configs
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
